eee3b2eb8c1d88a7b4349bb6870b2e1c29b45180
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-20 17:29:35 -0700

    update result adding logic

--- ex08/ft_atoi.c
+++ ex08/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 17:07:42 by seli              #+#    #+#             */
-/*   Updated: 2018/09/20 17:20:36 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/20 17:29:15 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ int	ft_atoi(char *str)
 	while (*str)
 	{
 		if (negative)
-			result += result * 10 - (*str - '0');
+			result = result * 10 - (*str - '0');
 		else
-			result += result * 10 + (*str - '0');
+			result = result * 10 + (*str - '0');
 		str++;
 	}
 	return (result);
