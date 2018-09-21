0c048d64ba06df4db15e173c4c099e8c7ec3afe7
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-20 17:57:34 -0700

    using old slow bubble sort atm

---
+++ ex09/ft_sort_integer_table.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_integer_table.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/20 17:30:29 by seli              #+#    #+#             */
+/*   Updated: 2018/09/20 17:57:09 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_ineger_table(int *tab, int size)
+{
+	int *end;
+	int *inner_head;
+	int *next_int;
+	int temp;
+
+	end = tab + size - 1;
+	while (tab < end)
+	{
+		inner_head = tab;
+		while (inner_head < end)
+		{
+			next_int = inner_head + 1;
+			if (*inner_head > *next_int)
+			{
+				temp = *inner_head;
+				*inner_head = *next_int;
+				*next_int = temp;
+			}
+			inner_head++;
+		}
+		end--;
+	}
+}
