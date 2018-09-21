c15640a478f767155344cbe24c99f283d15c5a1a
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-20 17:58:36 -0700

    update function name

--- ex09/ft_sort_integer_table.c
+++ ex09/ft_sort_integer_table.c
@@ -6,11 +6,11 @@
 /*   By: seli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 17:30:29 by seli              #+#    #+#             */
-/*   Updated: 2018/09/20 17:57:09 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/20 17:58:18 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_sort_ineger_table(int *tab, int size)
+void	ft_sort_integer_table(int *tab, int size)
 {
 	int *end;
 	int *inner_head;
